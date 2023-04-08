50d47b9d79f7c3d424ea5df9e8790e953eeb840e
Shashank Singh <shashank252127@gmail.com>
2023-04-08 15:54:07 +0530

    footer 1.0

--- libs/home-page/footer/footer.component.ts
+++ libs/home-page/footer/footer.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FOOTER_DATA } from '../configs/footer.config';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  footerData = FOOTER_DATA;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
