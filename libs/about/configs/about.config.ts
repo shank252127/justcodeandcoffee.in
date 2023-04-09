20f18664154fc3489f9fa061a6547727e9523834
Shashank Singh <shashank252127@gmail.com>
2023-04-09 22:13:20 +0530

    Creating about section

---
+++ libs/about/configs/about.config.ts
@@ -0,0 +1,11 @@
+export const ABOUT_DATA = {
+  PROFILE: 'Front-End Angular Developer',
+  INTRO:
+    "Hi, I'm Shashank Singh. A passionate Front-End Angular Developer @ Deloitte India, in Pune-India.",
+  SOCIAL_LINKS: ['LinkedIn', 'Github'],
+  PROFILE_IMAGE: 'assets/header_nav_logo.png',
+  TECH_STACK: {
+    SKILLED: ['Angular', 'JavaScript', 'TypeScript', 'HTML', 'CSS', 'SCSS'],
+    FAMILIAR_WITH: ['Java', 'Python', 'AngularJS', 'NodeJs', 'SQL', 'MongoDB'],
+  },
+};
