1800cd8aaeb69cd636c75248b79a7619fcaf63cb
Shashank Singh <shashank252127@gmail.com>
2023-04-12 23:39:16 +0530

    changes pushed 12/04/2023

---
+++ libs/home-page/home-page-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('../about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class HomePageRoutingModule {}
