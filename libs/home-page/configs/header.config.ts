0490fe5c8e56b321d4e30c6009e39eebd003586b
Shashank Singh <shashank252127@gmail.com>
2023-05-13 15:51:02 +0530

    changes

--- libs/home-page/configs/header.config.ts
+++ libs/home-page/configs/header.config.ts
@@ -3,8 +3,61 @@ export const HEADER_DATA = {
     LOGO_IMG: 'assets/header_nav_logo.png',
     APP_TITLE: 'justCodeAndCoffee.in',
   },
-  RIGHT_SIDE_MENU: ['About', 'Contact', 'Projects'],
-  ROUTER_LINK: ['about', 'contact', 'projects'],
+  RIGHT_SIDE_MENU: [
+    {
+      ID: 'ONE',
+      NAME: 'About',
+      ROUTE_PATH: 'about',
+      IS_NESTED: false,
+    },
+    {
+      ID: 2,
+      NAME: 'Contact',
+      ROUTE_PATH: 'contact',
+      IS_NESTED: false,
+    },
+    {
+      ID: 'THREE',
+      NAME: 'Dev. Tools',
+      ROUTE_PATH: 'projects',
+      IS_NESTED: true,
+    },
+    {
+      ID: 4,
+      NAME: 'Projects',
+      ROUTE_PATH: 'projects',
+      IS_NESTED: false,
+    },
+  ],
+  NESTED_MENU: {
+    THREE: [
+      {
+        NAME: 'JSON Formatter',
+        ROUTE_PATH: 'jsonformater',
+        ICON: 'assets/json-formatter.svg',
+      },
+      {
+        NAME: 'JSON Comp',
+        ROUTE_PATH: 'jsondiff',
+        ICON: 'assets/json-comp.svg',
+      },
+      {
+        NAME: 'JSON to String',
+        ROUTE_PATH: 'jsontostrong',
+        ICON: 'assets/string-compare.svg',
+      },
+      {
+        NAME: 'String Comp',
+        ROUTE_PATH: 'stringdiff',
+        ICON: 'assets/string-comp.svg',
+      },
+      {
+        NAME: 'Photo Resizer',
+        ROUTE_PATH: 'photoresizer',
+        ICON: 'assets/photo-resize.svg',
+      },
+    ],
+  },
 };
 
 //  RIGHT_SIDE_MENU: ['Home', 'Courses', 'Projects', 'About', 'Contact'],
--- libs/home-page/header/header.component.ts
+++ libs/home-page/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 // Constants
 import { HEADER_DATA } from '../configs/header.config';
 import { BREAKPOINT_VIEW } from 'global-styles/global-constants.config';
 import { Direction } from '@angular/cdk/bidi';
+import { MatMenuPanel } from '@angular/material/menu';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -15,9 +16,9 @@ export class HeaderComponent implements OnInit {
   mobTabStyleActive = false;
   // Mobile - Tablet Menu;
   activeHamburger = false;
-  dir: Direction = 'ltr';
-  @Output() menuOpened: EventEmitter<void> | undefined;
-  @Output() menuClosed: EventEmitter<void> | undefined;
+  dir: Direction = 'rtl';
+  subMenu: any;
+
   constructor(public breakpointObserver: BreakpointObserver) {
     this.breakpointObserver
       .observe([BREAKPOINT_VIEW.TAB_MOB_BP])
@@ -34,5 +35,14 @@ export class HeaderComponent implements OnInit {
 
   openHamburger(): void {
     this.activeHamburger = !this.activeHamburger;
+  }
+
+  setNestedMenuData(isNested: boolean, id: any): void {
+    if (isNested) {
+      this.subMenu = this.headerData.NESTED_MENU[id];
+    } else {
+      this.subMenu = [];
+    }
+    // console.log(this.menuData);
   }
 }
