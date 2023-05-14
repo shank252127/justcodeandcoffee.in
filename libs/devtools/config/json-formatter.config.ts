feab856295f4c632f9121d48b21d894ab1d63bbe
Shashank Singh <shashank252127@gmail.com>
2023-05-14 18:21:43 +0530

    accodian style changes

--- libs/devtools/config/json-formatter.config.ts
+++ libs/devtools/config/json-formatter.config.ts
@@ -1,5 +1,5 @@
 export const JSON_FORMATTER = {
-  HEADING: 'About APP',
+  HEADING: 'Description',
   ABOUT_APP:
     'This JSON formatter web app is a tool that helps to format and validate JSON data. It takes in a JSON string or object and formats it in a readable way with proper indentation and line breaks. It can also validate the JSON data and highlight any errors or syntax issues.',
   BTN_FORMAT: 'Format',
